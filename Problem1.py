6e0777f2a626f91a422fa5323c9038b6d5232d62
Youjin Jang <jangyoujin2003@gmail.com>
2024-03-18 19:50:36 +0900

    modified code

--- Problem1.py
+++ Problem1.py
@@ -2,11 +2,10 @@ import torch
 
 def train(x_train,label):
     ### Implement from here ###
-    
-    return w,b
+    w, b = 0, 0
+    return w, b
 
 def main():
-
     x_train = torch.tensor([0.0, 1.0, 2.0, 3.0, 4.0])
     label = torch.tensor([2.0, 4.0, 6.0, 8.0, 10.0])
     x_test = torch.tensor([5.0, 10.0, 8.0])
@@ -15,5 +14,3 @@ def main():
     
     y = w*x_test + b
     return y
-    
-main()
--- Tests/Test1.py
+++ Tests/Test1.py
@@ -1,7 +1,7 @@
 import Problem1
 import torch
 
-def test_Problem1():
+if __name__=="__main__":
     error = 1e-2
     answer = torch.tensor([12,22,18])
     assert(all(abs(answer - Problem1.main()) < error))        
