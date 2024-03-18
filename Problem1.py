901a16fd16e600c42835380e0110f2407315346b
Hyunseo Lee <72857353+leehyunseo03@users.noreply.github.com>
2024-03-18 14:39:45 +0900

    Problem1.py added

---
+++ Problem1.py
@@ -0,0 +1,17 @@
+import torch
+
+def train(x_train,label):
+    ### Implement from here ###
+    
+    return w,b
+
+if __name__ =="__main__":
+
+    x_train = torch.tensor([0.0, 1.0, 2.0, 3.0, 4.0])
+    label = torch.tensor([2.0, 4.0, 6.0, 8.0, 10.0])
+    x_test = torch.tensor([5.0, 10.0, 8.0])
+    
+    w,b = train(x_train, label) # Implement this
+    
+    y = w*x_test + b
+    print("answer : ", y)
