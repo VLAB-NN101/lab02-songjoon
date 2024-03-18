beca1a40bdff3888c58c3498ed9f5955fb6de160
Youjin Jang <jangyoujin2003@gmail.com>
2024-03-18 22:18:18 +0900

    writing csv

--- Problem1/Test.py
+++ Problem1/Test.py
@@ -1,6 +1,18 @@
 import Problem1.Problem as Problem
 import torch
+import csv
+import sys
+
+PATH = "~/Testcase/Lab02/Problem1/"
+def getAddress(x):
+    return PATH + "ex" + str(x) + ".csv"
 
 if __name__=="__main__":
     ## need to implement grading code
-    pass
+    ## test run example : ./Test.py 1
+    with open(getAddress(sys.argv[1]), "r") as f:
+        rdr = csv.reader(f)
+        arr = [line for line in rdr][1:]
+
+        train_data = arr[:-3]
+        test_data = arr[-3:]
