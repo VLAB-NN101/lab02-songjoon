618b7533c2064a303762488beed1dab32eb1c951
caleb@kaist.ac.kr <caleb@kaist.ac.kr>
2024-03-19 16:35:37 +0900

    Update Problem.py

--- Problem2/Problem.py
+++ Problem2/Problem.py
@@ -1,39 +1,28 @@
 import torch
-import pandas as pd
 
 ### Lab02 Problem 2
 ### Multiple Linear Regression - multiple variable
-### For given x_train(Hours, PrevStudy, Sleep, Sample) and label(Perf), perform linear regression.
+### For given x_train(x1,x2,...) and label(y), perform linear regression.
 ### Assume y = wx + b
 
-def train(Hours,PrevStudy,Sleep,Sample,Perf):
-    ### 2) Implement here ###
-
+def train(x_train, label):
+    ### Implement from here ###
     w = torch.tensor([0.])
     b = torch.tensor([0.])
     return w,b 
 
-def main():
-    PATH = "~/Testcase/Lab02/Problem2/"
-    data = pd.read_csv(PATH+'Student_Performance.csv')
-
-    ### 1) Implement here ###
-    Hours = None
-    PrevStudy = None
-    Sleep = None
-    Sample = None
-    Perf = None      # label
-    x_test = torch.FloatTensor([7,99,9,1])
+if __name__=="__main__":
+    x_train = torch.tensor([[0., 1.], [1., 0.], [2., 2.], [3., 1.], [4., 3.]])
+    label = torch.tensor([3., 2., 7., 6., 11.]) 
     
-    w,b = train(Hours,PrevStudy,Sleep,Sample,Perf) 
+    # y = x_0 + 2*x_1 + 1 # Note that not all test cases give clear line.
+    x_test = torch.tensor([[5., 3.], [10., 6.], [8., 9.]])
+    
+    w,b = train(x_train, label) 
     print("weight : " + str(w) + ", bias : " + str(b))
     
-    y = torch.dot(w,x_test) + b
+    y = torch.matmul(x_test,w) + b
     print("Predicted value : " + str(y))
     
-    answer = torch.tensor([91])
+    answer = torch.tensor([12, 23, 27])
     print("Actual value : " + str(answer))
-    
-    return y
-
-main()
