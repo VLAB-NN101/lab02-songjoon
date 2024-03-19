00cb8103532979581537a239749b3ec9b36b6c9a
Youjin Jang <jangyoujin2003@gmail.com>
2024-03-19 20:32:25 +0900

    test code updated

--- Problem1/Test.py
+++ Problem1/Test.py
@@ -12,27 +12,27 @@ def getAddress(x):
 if __name__ == "__main__":
     ## need to implement grading code
     ## test run example : ./Test.py 1
-    for num in range(1,4):
-        with open(getAddress(num), "r") as f:
-            rdr = csv.reader(f)
-            arr = [line for line in rdr][1:]
+    num = sys.argv[1]
+    with open(getAddress(num), "r") as f:
+        rdr = csv.reader(f)
+        arr = [line for line in rdr][1:]
 
-            train_data = arr[:-3]
-            test_data = arr[-3:]
+        train_data = arr[:-3]
+        test_data = arr[-3:]
             
-            ## process torch.Tensor and make comparsion
-            ## Answer should be < 1% relative error.
+        ## process torch.Tensor and make comparsion
+        ## Answer should be < 1% relative error.
             
-            train_tensor = torch.FloatTensor(np.float_(train_data))
-            x_train = train_tensor[:,0]
-            train_label = train_tensor[:,1]
-            w, b = Problem.train(x_train,train_label)
+        train_tensor = torch.FloatTensor(np.float_(train_data))
+        x_train = train_tensor[:,0]
+        train_label = train_tensor[:,1]
+        w, b = Problem.train(x_train,train_label)
             
-            test_tensor = torch.FloatTensor(np.float_(test_data))
-            x_test = test_tensor[:,0]
-            test_label = test_tensor[:,1]
-            y = w*x_test+b
+        test_tensor = torch.FloatTensor(np.float_(test_data))
+        x_test = test_tensor[:,0]
+        test_label = test_tensor[:,1]
+        y = w*x_test+b
             
-            error = 1e-1 * test_label
+        error = 1e-1 * test_label
 
-            assert(all(abs(test_label - y) < error)) 
+        assert(all(abs(test_label - y) < error)) 
